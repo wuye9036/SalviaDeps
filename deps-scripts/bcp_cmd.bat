dc8dbb96f5af828ff5094c27dc6a3e3b29c2769c
Ye Wu <wuye@microsoft.com>
2019-08-16 20:58:00 -0700

    Added new bcp.

--- deps-scripts/bcp_cmd.bat
+++ deps-scripts/bcp_cmd.bat
@@ -1,2 +1,4 @@
 mkdir boost-subset
 bcp --boost=.\boost_1_58_0 build test wave program_options property_tree signals2 locale inspect config circular_buffer spirit algorithm assign smart_ptr bimap logic phoenix uuid range utility icl chrono .\boost-subset
+
+bcp --boost=.\boost_1_70_0 build test wave program_options property_tree signals2 locale inspect config circular_buffer spirit algorithm assign bimap logic phoenix uuid range utility icl .\boost_1_70_0-subset
